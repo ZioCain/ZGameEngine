cefbecab38168e9bb92750b644850dc1bd85a0d7
Michele Castelletti <m.castelletti@sysdata.it>
2022-09-01 22:31:09 +0200

    Trying to use NPM to export package

---
+++ index.js
@@ -0,0 +1,25 @@
+const Extensions = require('./lib/extensions');
+const Vector2 = require('./lib/types');
+const Inputs = require('./lib/inputs');
+const Debug = require('./lib/logger');
+const ZGame = require('./lib/z-game');
+const Sprite = require('./lib/sprite');
+const Component = require('./lib/component');
+const Renderer = require('./lib/renderer');
+const Collider = require('./lib/collider');
+const Transform = require('./lib/transform');
+const RigidBody = require('./lib/rigid-body');
+
+module.exports = {
+	Extensions,
+	Vector2,
+	Inputs,
+	Debug,
+	ZGame,
+	Sprite,
+	Component,
+	Renderer,
+	Collider,
+	Transform,
+	RigidBody
+};
